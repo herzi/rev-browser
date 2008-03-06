0a5d5e86f653a4eca0c611c2fd67206e1f211baf
Sven Herzberg <sven@imendio.com>
2008-03-06 12:43:13 +0100

    added an horizontal box to the window

--- rev-browser.c
+++ rev-browser.c
@@ -28,12 +28,18 @@ main (int   argc,
       char**argv)
 {
 	GtkWidget* window;
+	GtkWidget* hbox;
 
 	gtk_init (&argc, &argv);
 
 	window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
 	g_signal_connect (window, "destroy",
 			  G_CALLBACK (gtk_main_quit), NULL);
+
+	hbox = gtk_hbox_new (FALSE, 0);
+	gtk_container_add (GTK_CONTAINER (window),
+			   hbox);
+
 	gtk_widget_show (window);
 
 	gtk_main ();
