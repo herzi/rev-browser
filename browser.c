2135c4d446915b527e50d2f54489b8bd50ae83bc
Sven Herzberg <sven@imendio.com>
2008-03-11 17:37:45 +0100

    use the new time bar widget

--- browser.c
+++ browser.c
@@ -24,6 +24,7 @@
 #include <gtk/gtk.h>
 
 #include "display.h"
+#include "time-bar.h"
 
 static void
 display_notify_can_step_left (GObject   * object,
@@ -123,7 +124,7 @@ main (int   argc,
 	g_signal_connect (window, "destroy",
 			  G_CALLBACK (gtk_main_quit), NULL);
 
-	hbox = gtk_hbox_new (FALSE, 0);
+	hbox = time_bar_new ();
 	gtk_container_add (GTK_CONTAINER (window),
 			   hbox);
 
