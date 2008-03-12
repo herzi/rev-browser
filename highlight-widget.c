6f91e533d2617f0b1bc8be97af46393ed8d10a80
Sven Herzberg <sven@imendio.com>
2008-03-12 17:51:43 +0100

    added a highlight pixbuf
    highlight-widget.c: added a GdkPixbuf to the GtkWidget that's as large as the
    widget's allocation; fill the GdkPixbuf with semi-transparent red

--- highlight-widget.c
+++ highlight-widget.c
@@ -25,9 +25,45 @@
 
 #include "highlight-widget.h"
 
+static GQuark highlight_pixbuf_quark = 0;
+
+static void
+update_highlight_pixbuf (GtkWidget* widget)
+{
+	GdkPixbuf* pixbuf = g_object_get_qdata (G_OBJECT (widget),
+					        highlight_pixbuf_quark);
+
+	if (!pixbuf ||
+	    widget->allocation.width != gdk_pixbuf_get_width (pixbuf) ||
+	    widget->allocation.height != gdk_pixbuf_get_height (pixbuf))
+	{
+		pixbuf = gdk_pixbuf_new (GDK_COLORSPACE_RGB,
+					 TRUE,
+					 8,
+					 widget->allocation.width,
+					 widget->allocation.height);
+
+		gdk_pixbuf_fill (pixbuf, 0xff00007f);
+
+		g_object_set_qdata_full (G_OBJECT (widget),
+					 highlight_pixbuf_quark,
+					 pixbuf,
+					 g_object_unref);
+	}
+}
+
 void
 highlight_widget (GtkWidget* widget)
 {
 	g_return_if_fail (GTK_IS_WIDGET (widget));
+
+	if (!highlight_pixbuf_quark) {
+		highlight_pixbuf_quark = g_quark_from_static_string ("RefBrowserHighlightPixbuf");
+	}
+
+	g_signal_connect_after (widget, "size-allocate",
+				G_CALLBACK (update_highlight_pixbuf), NULL);
+
+	update_highlight_pixbuf (widget);
 }
 
