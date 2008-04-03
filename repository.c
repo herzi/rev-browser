f75e9754abcf80c47219528ed2ed15b00b3f3081
Sven Herzberg <sven@imendio.com>
2008-04-03 17:37:01 +0200

    store the data in key-value objects (that we can properly order and put into a GSequence now)

--- repository.c
+++ repository.c
@@ -31,6 +31,42 @@ struct _RepositoryPrivate {
 
 #define PRIV(i) REPOSITORY(i)->_private
 
+/* Commits-per-Day Implementation */
+typedef struct {
+	gchar* day;
+	gint   num_commits;
+} CommitsPerDay;
+
+static CommitsPerDay*
+commits_per_day_new (gchar const* day)
+{
+	CommitsPerDay* result = g_slice_new (CommitsPerDay);
+	result->day = g_strdup (day);
+	result->num_commits = 1;
+	return result;
+}
+
+static void
+commits_per_day_free (CommitsPerDay* self)
+{
+	g_free (self->day);
+	g_slice_free (CommitsPerDay, self);
+}
+
+static guint
+commits_per_day_hash (gconstpointer key)
+{
+	return g_str_hash (((CommitsPerDay*)key)->day);
+}
+
+static gboolean
+commits_per_day_equal (gconstpointer v1,
+		       gconstpointer v2)
+{
+	return g_str_equal (((CommitsPerDay*)v1)->day,
+			    ((CommitsPerDay*)v2)->day);
+}
+
 /* GType Implementation */
 
 G_DEFINE_TYPE (Repository, repository, G_TYPE_OBJECT);
@@ -39,7 +75,10 @@ static void
 repository_init (Repository* self)
 {
 	PRIV(self) = G_TYPE_INSTANCE_GET_PRIVATE (self, TYPE_REPOSITORY, RepositoryPrivate);
-	PRIV(self)->num_commits_per_day = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, NULL);
+	PRIV(self)->num_commits_per_day = g_hash_table_new_full (commits_per_day_hash,
+								 commits_per_day_equal,
+								 (GDestroyNotify)commits_per_day_free,
+								 NULL);
 }
 
 static void
@@ -75,11 +114,19 @@ repository_new (void)
 		if (G_LIKELY (**iter)) {
 			if (!g_str_has_prefix (*iter, "commit ")) {
 				gchar** words = g_strsplit (*iter, " ", 2);
-				gsize count = GPOINTER_TO_SIZE (g_hash_table_lookup (PRIV(self)->num_commits_per_day, words[0]));
-				count++;
-				g_hash_table_insert (PRIV(self)->num_commits_per_day,
-						     g_strdup (words[0]),
-						     GSIZE_TO_POINTER (count));
+				CommitsPerDay* cpd = commits_per_day_new (words[0]);
+				CommitsPerDay* match = g_hash_table_lookup (PRIV(self)->num_commits_per_day,
+									    cpd);
+
+				if (match) {
+					match->num_commits += cpd->num_commits;
+					commits_per_day_free (cpd);
+				} else {
+					g_hash_table_insert (PRIV(self)->num_commits_per_day,
+							     cpd,
+							     cpd);
+				}
+
 				g_strfreev (words);
 			}
 		}
@@ -88,13 +135,28 @@ repository_new (void)
 	return self;
 }
 
+struct GHFuncAndUserData {
+	GHFunc   func;
+	gpointer user_data;
+};
+
+static void
+translate_from_cpd_to_expected (gconstpointer key,
+				gconstpointer value,
+				struct GHFuncAndUserData* data)
+{
+	CommitsPerDay const* cpd = key;
+	data->func (cpd->day, GINT_TO_POINTER (cpd->num_commits), data->user_data);
+}
+
 void
 repository_foreach (Repository* self,
 		    GHFunc      func,
 		    gpointer    user_data)
 {
+	struct GHFuncAndUserData data = {func, user_data};
 	g_hash_table_foreach (PRIV(self)->num_commits_per_day,
-			      func,
-			      user_data);
+			      (GHFunc)translate_from_cpd_to_expected,
+			      &data);
 }
 
